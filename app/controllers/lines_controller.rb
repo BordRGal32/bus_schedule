3f28ea8dec777534225cfeca4841f536e9fc6a95
Lauren Yeiser & vincent rotter <student@epicodus.com>
2014-04-07 17:07:45 -0700

    add stops

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -16,7 +16,21 @@ def index
     @line = Line.find(params[:id])
   end
   def update
-    @line = Line.find(params[:id])
+      @line = Line.find(params[:id])
+      if params[:station_ids] != nil
+        stations = params[:station_ids]
+        stations.each do |id|
+          station = Station.find(id)
+          @line.stations << station
+        end
+      end
+      if params[:station_delete_ids] != nil
+        delete_stations = params[:station_delete_ids]
+        delete_stations.each do |id|
+          station = Station.find(id)
+          @line.stations.delete(station)
+        end
+      end
     if @line.update(line_params)
       redirect_to line_path
     else
